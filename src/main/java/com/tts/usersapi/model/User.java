e44f320dc60b12d418dc5cbbbee6e9066762aa25
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-18 14:31:53 -0500

    Add validation annotations to User model

--- src/main/java/com/tts/usersapi/model/User.java
+++ src/main/java/com/tts/usersapi/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +21,10 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Size(max=20)
 	private String firstName;
+	@Size(min=2)
 	private String lastName;
+	@Size(min=4, max=20)
 	private String stateOfResidence;
 }
