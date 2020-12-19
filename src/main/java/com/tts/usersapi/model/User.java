8c1d48114d69abd4b4440c91bd8844b89ba3b732
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-18 21:01:58 -0500

    Add annotations to describe each property

--- src/main/java/com/tts/usersapi/model/User.java
+++ src/main/java/com/tts/usersapi/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,11 +21,15 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(notes = "The auto-generated id of the user")
 	private Long id;
 	@Size(max=20)
+	@ApiModelProperty(notes = "The first name of the user")
 	private String firstName;
 	@Size(min=2)
+	@ApiModelProperty(notes = "The last name of the user")
 	private String lastName;
 	@Size(min=4, max=20)
+	@ApiModelProperty(notes = "The (US) state of residence of the user")
 	private String stateOfResidence;
 }
